766029e4702eaf1616053caa4ad70bacacadb035
johannes67890 <johannes@orager.dk>
2021-05-03 20:26:44 +0200

    added buy sell function

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,13 @@
+amount = (100000000000).toLocaleString("ja-JP"); /*amount balance avaliable. toLocaleString("ja-JP") for the comma*/ 
 function balance()
 {
-    amount = (100000000000).toLocaleString("ja-JP"); /*amount balance avaliable. toLocaleString("ja-JP") for the comma*/ 
     document.getElementById("money").innerHTML =  "$" + amount; /*prints to HTML*/
+}
+
+function sell_buy(opr, val)
+{
+    var x = (opr == 'sell') ? +val : -val; /* conditional */
+    new_amount = Number(amount.replace(/,/g, '')) + x; /*change 'var_amount' to number and plus with paramater*/
+    amount = new_amount.toLocaleString("ja-JP"); /* add back comma on 'amount' */
+    document.getElementById("money").innerHTML =  "$" + amount; /* print to screen */
 }
