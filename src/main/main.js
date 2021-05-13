a0105baa966a17fcaa67435d3e2e456b6efaff7d
johannes67890 <johannes@orager.dk>
2021-05-13 18:07:49 +0200

    fixed input system

--- src/main/main.js
+++ src/main/main.js
@@ -5,17 +5,18 @@ function balance()
     document.getElementById("money").innerHTML =  "$" + amount; /*prints to HTML*/
 }
 
-function sell_buy(opr, val)
+function sell_buy(opr, val, input_id)
 {
     var x = (opr == 'sell') ? +val : -val; /* conditional */
     new_amount = Number(amount.replace(/,/g, '')) + x; /*change 'var_amount' to number and plus with paramater*/
     amount = new_amount.toLocaleString("ja-JP"); /* add back comma on 'amount' */
     document.getElementById("money").innerHTML =  "$" + amount; /* print to screen */
-}
-
-function item_count()
-{
-    document.getElementById('2').value = "test"
+    //
+    console.log(document.getElementById(input_id).value); //
+    let input_amount = document.getElementById(input_id).value;
+    input_amount = Number(input_amount) + ((opr == 'sell') ? -1 : +1) ;
+    console.log(input_amount); //
+    document.getElementById(input_id).value = input_amount;
 }
 
 function balance_error()
@@ -24,8 +25,17 @@ function balance_error()
     {
         console.log(amount);
         alert("You are over the balance limit! The page wil reload in a moment!")
-            setTimeout(() => {
-                location.reload();
-            }, 2000);
+        setTimeout(() => {
+            location.reload();
+        }, 2000);
     }
- }
+}
+/*
+function item_count(input_id)
+{
+    console.log(document.getElementById(input_id).value);
+    let input = document.getElementById(input_id).value;
+    input = Number(input) + 1;
+    document.getElementById(input_id).innerText = input;
+    console.log(input);
+}*/
