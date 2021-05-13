db33472a66f249a62dde299328b6ba429ba254db
johannes67890 <johannes@orager.dk>
2021-05-13 19:45:46 +0200

    work on optimize code

--- src/main/main.js
+++ src/main/main.js
@@ -7,11 +7,12 @@ function balance()
 
 function sell_buy(opr, val, input_id)
 {
+    //change balance amount
     var x = (opr == 'sell') ? +val : -val; /* conditional */
     new_amount = Number(amount.replace(/,/g, '')) + x; /*change 'var_amount' to number and plus with paramater*/
     amount = new_amount.toLocaleString("ja-JP"); /* add back comma on 'amount' */
     document.getElementById("money").innerHTML =  "$" + amount; /* print to screen */
-    //
+    //change input amount
     console.log(document.getElementById(input_id).value); //
     let input_amount = document.getElementById(input_id).value;
     input_amount = Number(input_amount) + ((opr == 'sell') ? -1 : +1) ;
@@ -30,6 +31,37 @@ function balance_error()
         }, 2000);
     }
 }
+
+let itemlist = [{
+    name: "pizza",
+    price: 10,
+    imgDir: "../img/pizza.png",
+}]
+
+function generateLayout()
+{
+    console.log("layout generated");
+    itemlist.map((item, index) => {
+        layout(index, item.name, item.price, item.imgDir);
+    })
+}
+
+function layout(parent, name,price,imgDir)
+{
+    var n = document.createElement("div");
+    n.innerHTML = name;
+    n.classList.add("item_text");
+    var element = document.getElementById(parent);
+    parent.appendChild(n);
+
+    /*
+    var img = document.createElement("img");
+    img.src = imgDir;
+    img.classList.add("item_img");
+    img = document.getElementById("item1");
+    img.appendChild(src); */
+}
+
 /*
 function item_count(input_id)
 {
